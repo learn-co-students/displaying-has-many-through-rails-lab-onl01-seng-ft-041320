73485269bcae33ac4c177123293048938dad21e5
osha7 <ogroetz@gmail.com>
2020-06-17 13:00:10 -0600

    Done.

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,11 @@
+class AppointmentsController < ApplicationController
+
+    def index
+        @appointments = Appointment.all 
+    end
+
+    def show
+        @appointment = Appointment.find_by(id: params[:id])
+    end
+
+end
