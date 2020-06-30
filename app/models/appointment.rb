736f2367e7d701a29b2510d538d7f766ac25bf65
Franklyn Rodriguez <franklynr92@gmail.com>
2020-06-29 22:20:10 -0400

    Done.

---
+++ app/models/appointment.rb
@@ -0,0 +1,13 @@
+class Appointment < ApplicationRecord
+    belongs_to :patient
+    belongs_to :doctor
+
+    def doctor_name=(name)
+        self.doctor = Doctor.find_or_create_by(name: name)
+    end
+
+    def doctor_name
+        self.doctor ? self.doctor.name : nil
+    end
+
+end
---
+++ db/migrate/20200629234629_doctor.rb
@@ -0,0 +1,9 @@
+class Doctor < ActiveRecord::Migration[5.0]
+  def change
+    create_table :doctors do |t|
+      t.string :name
+      t.string :department
+    end
+
+  end
+end
